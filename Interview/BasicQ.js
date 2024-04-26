3dee61ea23dba7cd7c2b59e3ce4fcc4f22252219
SHEIKH MAJID TARIQUE <shaikhmajid321786@gmail.com>
2024-04-27 02:19:44 +0530

    interview Question

---
+++ Interview/BasicQ.js
@@ -0,0 +1,15 @@
+// // Define React
+// React.js ek open - source JavaScript library hai jo user interfaces banane mein madad karta hai.Ye Facebook ne develop kiya hai aur ab ek widespread community ke support mein hai.
+// >> React makes thing easy and used build complex fronted
+
+
+// Component-Based Architecture: React.js component-based architecture follow karta hai,
+//  jismein UI ko chote chote components mein divide kiya jata hai jo reusable aur maintainable hote hain.
+
+
+// JSX
+
+// JSX (JavaScript XML) ek syntax extension hai jo JavaScript ke sath XML-like syntax provide karta hai. Iska matalab hai aap JavaScript code mein XML-like tags ka istemal kar sakte hain jo phir JavaScript expressions ke saath combine ho jate hain.
+
+// JSX ke madhyam se, aap UI elements ko ek intuitive aur declarative tareeke se define kar sakte hain. Yeh React mein UI components ko define karne ke liye use hota hai. JSX ke through, aap HTML tags aur JavaScript expressions ko ek saath likh
+//  sakte hain, jo development ko simplify karta hai.
